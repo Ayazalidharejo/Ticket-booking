b16c9b7b0a9bcd80fbe076d6207893c54b1df43e
Ayazalidharejo <ayazali.xwave@gmail.com>
2025-04-15 16:18:26 +0500

    change

--- src/App.js
+++ src/App.js
@@ -668,7 +668,7 @@ import axios from 'axios';
 import image from "./image/img.jpg"; // Make sure this image exists in your project
 
 // API base URL - update this to match your backend
-const API_URL = 'https://booking-backend-lilac.vercel.app/';
+const API_URL = 'https://booking-backend-lilac.vercel.app';
 
 
 
@@ -821,7 +821,7 @@ const App = () => {
     e.preventDefault();
     try {
       setLoading(true);
-      const res = await axios.post("https://vercel.com/ayazalidharejos-projects/booking-backend/api/users/register", {
+      const res = await axios.post(`${API_URL}api/users/register`, {
         username: formData.username,
         email: formData.email,
         password: formData.password,
